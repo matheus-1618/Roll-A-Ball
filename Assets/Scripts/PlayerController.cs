a74f4f265fbedeb576c9eacdd00a54cee80125b9
matheus-1618 <matheus1207live@gmail.com>
2023-09-09 19:25:37 -0300

    Add triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,4 +28,13 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
+    }
+
 }
